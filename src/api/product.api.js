cf17e564368109ab726cdd50d5df345207e95c04
Shalaka Patil <shalu214patil@gmail.com>
2024-08-29 11:10:14 +0530

    Scanner fix for product.api.js

--- src/api/product.api.js
+++ src/api/product.api.js
@@ -5,60 +5,65 @@ import { getCall } from "./axios";
  * @returns
  */
 export const getAllProductRequest = (params) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const data = await getCall(`/clientApis/v2/search`, params);
-            return resolve(data.response);
-        } catch (err) {
-            return reject(err);
-        }
-    });
+  return new Promise(async (resolve, reject) => {
+    try {
+      const data = await getCall(`/clientApis/v2/search`, params);
+      return resolve(data.response);
+    } catch (err) {
+      return reject(err);
+    }
+  });
 };
 
 /**
  * function to get all filters
  * @returns
  */
-export const getAllFiltersRequest = (subCatName=null, providerId=null) => {
-    let params = {};
-    if(subCatName){
-        let subCategoryName = subCatName.replace("And", "&");
-        params.category = subCategoryName;
+export const getAllFiltersRequest = (subCatName = null, providerId = null) => {
+  let params = {};
+  if (subCatName) {
+    let subCategoryName = subCatName.replace("And", "&");
+    params.category = subCategoryName;
+  }
+  if (providerId) {
+    params.provider = providerId;
+  } else {
+  }
+  return new Promise(async (resolve, reject) => {
+    try {
+      const data = await getCall(`/clientApis/v2/attributes`, params);
+      return resolve(data.response);
+    } catch (err) {
+      return reject(err);
     }
-    if(providerId){
-        params.provider = providerId;
-    }else{}
-    return new Promise(async (resolve, reject) => {
-        try {
-            const data = await getCall(`/clientApis/v2/attributes`, params);
-            return resolve(data.response);
-        } catch (err) {
-            return reject(err);
-        }
-    });
+  });
 };
 
 /**
  * function to get all filters
  * @returns
  */
-export const getAllFilterValuesRequest = (attributeCode, subCatName = null, providerId = null) => {
-    let params = {
-        attribute_code: attributeCode,
-    };
-    if(subCatName){
-        let subCategoryName = subCatName.replace("And", "&");
-        params.category = subCategoryName;
+export const getAllFilterValuesRequest = (
+  attributeCode,
+  subCatName = null,
+  providerId = null
+) => {
+  let params = {
+    attribute_code: attributeCode,
+  };
+  if (subCatName) {
+    let subCategoryName = subCatName.replace("And", "&");
+    params.category = subCategoryName;
+  }
+  if (providerId) {
+    params.provider = providerId;
+  }
+  return new Promise(async (resolve, reject) => {
+    try {
+      const data = await getCall(`/clientApis/v2/attributeValues`, params);
+      return resolve(data.response);
+    } catch (err) {
+      return reject(err);
     }
-    if(providerId){
-        params.provider = providerId;
-    }else{}
-    return new Promise(async (resolve, reject) => {
-        try {
-            const data = await getCall(`/clientApis/v2/attributeValues`, params);
-            return resolve(data.response);
-        } catch (err) {
-            return reject(err);
-        }
-    });
+  });
 };
